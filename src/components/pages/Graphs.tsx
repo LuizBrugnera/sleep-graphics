9dbb2c122500bc585a0a54431572bdd5c966d2d0
Luiz Brugnera <luiz.r.brugnera@gmail.com>
2023-09-06 18:01:17 -0300

    fix - import not used

--- src/components/pages/Graphs.tsx
+++ src/components/pages/Graphs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { SleepDataService } from "../services/SleepDataService";
 import { SleepDataAllPersonsDTO } from "../dto/SleepDataDto";
 import * as S from "../../styles";
